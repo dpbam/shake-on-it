e584d89428397438919bd8f20de8e0a680061cbf
dpbam <derekphtpt@gmail.com>
2021-08-18 17:20:12 -0600

    deleted the user-rating.handlebars file

--- public/javascript/user-rating.js
+++ public/javascript/user-rating.js
@@ -1,6 +1,6 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function ratingFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
