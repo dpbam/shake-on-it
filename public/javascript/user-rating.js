705504efcf88133d6fc9d9dbd01266620298fca9
Clark <81452611+freezenleo@users.noreply.github.com>
2021-08-19 19:38:59 -0500

    update

--- public/javascript/user-rating.js
+++ public/javascript/user-rating.js
@@ -1,45 +1,29 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-// function ratingFunction() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// }
-
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function (event) {
-
-//   if (!event.target.matches(".userDropbtn")) {
-//     var dropdowns = document.getElementsByClassName("ratingDropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains("show")) {
-//         openDropdown.classList.remove("show");
-//       }
-//     }
-//   }
-// };
 async function ratingFormHandler(event) {
   event.preventDefault();
 
   const select = document.querySelector("#rating");
   const option = select.options[select.selectedIndex].value;
-
+  console.log('response', option);
   const id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
 
+  console.log('response', id);
   if (option) {
+    console.log('response', option);
     const response = await fetch(`/api/posts/${id}/rating`, {
-      method: "PUT",
+      method: 'PUT',
       body: JSON.stringify({
         post_id: id,
-        num_rating: option,
+        num_rating: option
       }),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
+    console.log('response', response);
     if (response.ok) {
+      console.log('response', response);
       document.location.reload();
     } else {
       alert(response.statusText);
@@ -47,4 +31,4 @@ async function ratingFormHandler(event) {
   }
 }
 
-document.querySelector("#rating").addEventListener("change", ratingFormHandler);
+document.querySelector('#rating').addEventListener('change', ratingFormHandler);
