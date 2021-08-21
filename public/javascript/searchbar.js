9ac51f501fcb5a52941aa4873683eb75e09495d2
Clark <81452611+freezenleo@users.noreply.github.com>
2021-08-20 21:09:16 -0500

    update

--- public/javascript/searchbar.js
+++ public/javascript/searchbar.js
@@ -1,26 +1,50 @@
-async function searchProduct() {
+const { response } = require("express");
+
+function searchProduct() {
     let input = document.getElementById('searchbar').value;
     input = input.toLowerCase();
 
-    const response = await fetch('/api/posts', {
+    fetch('http://localhost:3001/api/posts', {
         method: 'GET',
-        body: JSON.stringify({}),
+        // body: JSON.stringify({}),
         headers: { 'Content-Type': 'application/json' },
     })
-    if (response.ok) {
-        let x = document.querySelector('#post-list').value;
-        for (i = 0; i < x.length; i++) {
-            if (!x[i].innerHTML.toLowerCase().includes(input)) {
-                x[i].style.display = "none";
-            }
-            else {
-                x[i].style.display = "block";
-            }
-        }
-        document.location.replace('homepage');
-    }
+        .then(response => {
+            return response.json();
+            // console.log(response);
+        })
+        .then(dbPostData => {
 
+            for (i = 0; i < dbPostData.length; i++) {
+                let x = dbPostData[i].title;
+                if (x.includes(input)) {
+                    const posts = dbPostData.map((post) => post.get({ plain: true }));
+                    res.render("homepage", {
+                        posts,
+                        loggedIn: req.session.loggedIn,
+                    });
+                }
+            }
+        })
 }
+// if (response.ok) {
+//     let x = ;
+//     for (i = 0; i < x.length; i++) {
+//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//             x[i].style.display = "none";
+//         }
+//         else {
+//             x[i].style.display = "block";
+//         }
+//     }
+//     document.location.replace('homepage');
+// }
 
 
-document.getElementById('searchbar').addEventListener('keyup', searchProduct);
+
+
+document.getElementById('searchbar').addEventListener('keypress', function (e) {
+    if (e.key == 'Enter') {
+        searchProduct();
+    }
+});
